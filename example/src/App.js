997b67fdc7052683a57138a0107c0c5725de3a63
Ryan Harris <harrisryan1@gmail.com>
2020-05-10 20:29:04 -0400

    feat: useDatabase and useGetDocument

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-import { useMyHook } from 'use-fauna'
+import { useDatabase, useGetDocument } from "use-fauna";
 
 const App = () => {
-  const example = useMyHook()
-  return (
-    <div>
-      {example}
-    </div>
-  )
-}
-export default App
+  const db = useDatabase("fnADrW9uexACE1_GWGovu3My4mXWcm-tgQ3Sp3oP");
+  console.log(db);
+  const [doc, status] = useGetDocument(db, "orders", "264990427463026195");
+  console.log("doc", doc);
+  console.log("status", status);
+  return <div>App</div>;
+};
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,51 @@
-import * as React from 'react';
+import { useEffect, useState } from "react";
+import faunadb from "faunadb";
+const { query: q } = faunadb;
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
-
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
-
-  return counter;
+const STATUS = {
+  NOT_LOADED: "not-loaded",
+  LOADING: "loading",
+  LOADED: "loaded",
+  ERROR: "error",
 };
+
+interface Ref {
+  id: string;
+}
+
+interface Document {
+  ref: Ref;
+  ts: number;
+  data: object;
+}
+
+export function useDatabase(faunaKey: string) {
+  return new faunadb.Client({ secret: faunaKey });
+}
+
+export function useGetDocument(
+  db: faunadb.Client,
+  collectionName: string,
+  refId: string
+): object {
+  const [status, setStatus] = useState(STATUS.NOT_LOADED);
+  const [document, setDocument] = useState<null | Document>(null);
+
+  useEffect(() => {
+    const request = db.query(q.Get(q.Ref(q.Collection(collectionName), refId)));
+
+    request
+      .then(async (res) => {
+        setStatus(STATUS.LOADING);
+        setDocument(await res);
+      })
+      .then(() => {
+        setStatus(STATUS.LOADED);
+      })
+      .catch((err) => {
+        console.error(`[fauna-hooks] ${err}`);
+        setStatus(STATUS.ERROR);
+      });
+  }, []);
+  return [document, status];
+}
