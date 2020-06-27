96f1117f49f257e9c0b64e51549f58979a72189f
Ryan Harris <harrisryan1@gmail.com>
2020-06-26 21:28:17 -0400

    clean up examples

--- example/src/App.js
+++ example/src/App.js
@@ -13,32 +13,25 @@ const App = () => {
   const db = useDatabase('fnADrW9uexACE1_GWGovu3My4mXWcm-tgQ3Sp3oP')
   // console.log(db)
 
-  // const [getAllDocuments, doc, status] = useGetAllDocuments(db)
-
-  // const [getDocument, doc, status] = useGetDocument(db)
-
-  const [createDocument, doc, status] = useCreateDocument(db)
-
-  // const [deleteDocument, doc, status] = useDeleteDocument(db)
-
-  // const [updateDocument, doc, status] = useUpdateDocument(db)
-
-  console.log('doc', doc)
-  console.log('status', status)
+  const [getAllDocuments, allDocs, status] = useGetAllDocuments(db)
+  const [getDocument, singleDoc, status] = useGetDocument(db)
+  const [createDocument, createdDoc, status] = useCreateDocument(db)
+  const [deleteDocument, deletedDoc, status] = useDeleteDocument(db)
+  const [updateDocument, updatedDoc, status] = useUpdateDocument(db)
 
   return (
     <div>
-      {/* <button onClick={() => getAllDocuments('storehouses', 2)}>Get all docs</button> */}
-      {/* <button onClick={() => getDocument('storehouses', '264990427443102227')}>
+      <button onClick={() => getAllDocuments('storehouses', 2)}>Get all docs</button>
+      <button onClick={() => getDocument('storehouses', '264990427443102227')}>
         Get single doc
-      </button> */}
+      </button>
       <button onClick={() => createDocument('storehouses', { name: 'john mayer' })}>
         Create single doc
       </button>
-      {/* <button onClick={() => deleteDocument('storehouses', '269443476876165650')}>
+      <button onClick={() => deleteDocument('storehouses', '269443476876165650')}>
         Delete single doc
-      </button> */}
-      {/* <button
+      </button>
+      <button
         onClick={() =>
           updateDocument('storehouses', '269443543992369683', {
             band: 'funky'
@@ -46,7 +39,7 @@ const App = () => {
         }
       >
         Update document
-      </button> */}
+      </button>
     </div>
   )
 }
