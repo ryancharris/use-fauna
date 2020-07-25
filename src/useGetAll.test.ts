71651cd775da9522d10c347d20b889e06d7354eb
Ryan Harris <harrisryan1@gmail.com>
2020-07-25 18:56:31 -0400

    test: add useGetAll tests

--- src/useGetAll.test.ts
+++ src/useGetAll.test.ts
@@ -10,7 +10,7 @@ describe('useGetAll', () => {
   it('gets all collections successfully', async () => {
     // Instantiate access to DB
     const { result: db } = renderHook(() =>
-      useFaunaClient('fnADxiwdnBACEyA9iCmaMjZ_Rs9Nb4xh3x8Wve70')
+      useFaunaClient('fnADxo6lvWACEnAmge1HiGgF9FmI48Ok75ij_Yfk')
     )
     const client = db.current
     expect(client).toBeInstanceOf(faunadb.Client)
@@ -23,106 +23,45 @@ describe('useGetAll', () => {
 
     expect(getAll).toBeInstanceOf(Function)
     expect(data).toBeNull()
-    expect(status).toEqual(FaunaStatus.NOT_LOADED)
 
-    await act(async () => {
+    act(async () => {
       getAll('databases')
 
       await waitForNextUpdate()
       expect(data).toBeNull()
+      expect(status).toEqual(FaunaStatus.NOT_LOADED)
+
+      await waitForNextUpdate()
       expect(status).toEqual(FaunaStatus.LOADING)
 
       await waitForNextUpdate()
-      expect(data).toBeDefined()
-      console.log(data)
-      expect(data).toBeInstanceOf(Array)
       expect(status).toEqual(FaunaStatus.LOADED)
+      expect(data).toBeDefined()
+      expect(data).toBeInstanceOf(Array)
     })
   })
 
-  // it('fails get all documents', async () => {
-  //   // Instantiate access to DB
-  //   const { result: db } = renderHook(() =>
-  //     useFaunaClient('fnADrW9uexACE1_GWGovu3My4mXWcm-tgQ3Sp3oP')
-  //   )
-  //   const database = db.current
-  //   expect(database).toBeInstanceOf(faunadb.Client)
+  it('fails get all documents', async () => {
+    // Instantiate access to DB
+    const { result: db } = renderHook(() =>
+      useFaunaClient('fnADrW9uexACE1_GWGovu3My4mXWcm-tgQ3Sp3oP')
+    )
+    const client = db.current
+    expect(client).toBeInstanceOf(faunadb.Client)
 
-  //   // Render useGetAllDocuments
-  //   const { result, waitForNextUpdate } = renderHook(() => useGetAllDocuments(database))
-  //   const getAllDocuments = await result.current[0]
-  //   const doc = result.current[1]
-  //   const status = result.current[2]
+    // Render useGetAll
+    const { result, waitForNextUpdate } = renderHook(() => useGetAll(client))
+    const getAll = await result.current[0]
+    const data = result.current[1]
+    const status = result.current[2]
 
-  //   act(async () => {
-  //     getAllDocuments('non-existent-collection')
+    act(async () => {
+      getAll('non-existent-schema')
 
-  //     // Fails to fetch data and resolves to ERROR
-  //     await waitForNextUpdate()
-  //     expect(doc).toBeNull()
-  //     expect(status).toEqual(FaunaStatus.ERROR)
-  //   })
-  // })
+      // Fails to fetch data and resolves to ERROR
+      await waitForNextUpdate()
+      expect(data).toBeNull()
+      expect(status).toEqual(FaunaStatus.ERROR)
+    })
+  })
 })
-
-// describe('useGetAllDocuments', () => {
-//   it('gets all documents successfully', async () => {
-//     // Instantiate access to DB
-//     const { result: db } = renderHook(() =>
-//       useFaunaClient('fnADrW9uexACE1_GWGovu3My4mXWcm-tgQ3Sp3oP')
-//     )
-//     const database = db.current
-//     expect(database).toBeInstanceOf(faunadb.Client)
-
-//     // Render useGetAllDocuments
-//     const { result, waitForNextUpdate } = renderHook(() => useGetAllDocuments(database))
-//     const getAllDocuments = await result.current[0]
-//     const doc = await result.current[1]
-//     const status = await result.current[2]
-
-//     expect(getAllDocuments).toBeInstanceOf(Function)
-//     expect(doc).toBeNull()
-//     expect(status).toEqual(FaunaStatus.NOT_LOADED)
-
-//     // Trigger GET request
-//     act(async () => {
-//       getAllDocuments('storehouses', 2)
-
-//       // Changes to LOADING state
-//       await waitForNextUpdate()
-//       expect(doc).toBeNull()
-//       expect(status).toEqual(FaunaStatus.LOADING)
-
-//       // Resolves with Document and LOADED status
-//       await waitForNextUpdate()
-//       expect(doc).toBeDefined()
-//       expect(doc).toBeInstanceOf(Array)
-//       expect(doc.length).toBe(2)
-//       expect(status).toEqual(FaunaStatus.LOADED)
-//     })
-//   })
-
-//   it('fails get all documents', async () => {
-//     // Instantiate access to DB
-//     const { result: db } = renderHook(() =>
-//       useFaunaClient('fnADrW9uexACE1_GWGovu3My4mXWcm-tgQ3Sp3oP')
-//     )
-//     const database = db.current
-//     expect(database).toBeInstanceOf(faunadb.Client)
-
-//     // Render useGetAllDocuments
-//     const { result, waitForNextUpdate } = renderHook(() => useGetAllDocuments(database))
-//     const getAllDocuments = await result.current[0]
-//     const doc = result.current[1]
-//     const status = result.current[2]
-
-//     act(async () => {
-//       getAllDocuments('non-existent-collection')
-
-//       // Fails to fetch data and resolves to ERROR
-//       await waitForNextUpdate()
-//       expect(doc).toBeNull()
-//       expect(status).toEqual(FaunaStatus.ERROR)
-//     })
-//   })
-// })
