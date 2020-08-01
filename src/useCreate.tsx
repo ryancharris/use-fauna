0b0a3f3a5cd3ca25a77b061480774535ba3fd92c
Ryan Harris <harrisryan1@gmail.com>
2020-08-01 12:29:08 -0400

    feat: add states, update types

--- src/useCreate.tsx
+++ src/useCreate.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react'
 import faunadb from 'faunadb'
 
-export default function useCreate(client: faunadb.Client): [Function, string, string] {
+import { FaunaStatus } from './constants'
+import { DataItem } from './types/fauna'
+
+export default function useCreate(client: faunadb.Client): [Function, null | DataItem, string] {
+  const [data, setData] = useState(null)
+  const [status, setState] = useState(FaunaStatus.NOT_LOADED)
+
   console.log(client)
-  return [() => {}, '', '']
+  return [() => {}, data, status]
 }
