2b95294f4612cfbcc6a261c7a55196bee5b1ca83
Ryan Harris <harrisryan1@gmail.com>
2020-08-01 12:55:27 -0400

    feat: wire up hook for Collections

--- example/src/Content.tsx
+++ example/src/Content.tsx
@@ -11,20 +11,22 @@ function Content() {
   const [getAllInputValue, setGetAllInputValue] = useState('collections')
   const [collection, setCollection] = useState('')
   const [getAll, data = null, status] = useGetAll(client)
-  console.log('data', data)
-  console.log('status', status)
+  // console.log('data', data)
+  // console.log('status', status)
 
   // useGet
   const [getFunction, getData, getStatus] = useGet(client)
   const [getSchema, setGetSchema] = useState('collection')
   const [getRefId, setGetRefId] = useState('')
   const [useGetRefId, setUseGetRefId] = useState('')
-  console.log('getFunction', getFunction)
-  console.log('getData', getData)
-  console.log('getStatus', getStatus)
+  // console.log('getFunction', getFunction)
+  // console.log('getData', getData)
+  // console.log('getStatus', getStatus)
 
   // useCreate
   const [createFunction, createData, createStatus] = useCreate(client)
+  const [createSchema, setCreateSchema] = useState('collection')
+  const [createSchemaName, setCreateSchemaName] = useState('')
   console.log('createFunction', createFunction)
   console.log('createData', createData)
   console.log('createStatus', createStatus)
@@ -33,7 +35,7 @@ function Content() {
     <div>
       {/* useGetAll */}
       <div>
-        <p>Get all of a schema type</p>
+        <h2>useGetAll</h2>
         <select
           name="schema"
           id="schema"
@@ -82,6 +84,7 @@ function Content() {
 
       {/* useGet */}
       <div>
+        <h2>useGet</h2>
         <div>
           <label htmlFor="getRef">
             {getSchema !== 'document' ? `${getSchema}` : 'collection'} name
@@ -131,6 +134,52 @@ function Content() {
             <code>{JSON.stringify(getData)}</code>
           </div>
         )}
+      </div>
+
+      <hr />
+
+      {/* useCreate */}
+      <div>
+        <h2>useCreate</h2>
+        <div>
+          <select
+            name="createSchema"
+            id="createSchema"
+            value={createSchema}
+            onChange={e => {
+              setCreateSchema(e.target.value)
+            }}
+            defaultValue={0}
+          >
+            <option value="collection">collection</option>
+            <option value="database">database</option>
+            <option value="document">document</option>
+            <option value="function">function</option>
+            <option value="index">index</option>
+          </select>
+          <div>
+            <label htmlFor="createSchemaName">Name:</label>
+            <input
+              type="text"
+              id="createSchemaName"
+              value={createSchemaName}
+              onChange={e => setCreateSchemaName(e.target.value)}
+            />
+            <button
+              onClick={e => {
+                e.preventDefault()
+                createFunction(createSchema, { name: createSchemaName })
+              }}
+            >
+              Create
+            </button>
+            {createData && (
+              <div>
+                <code>{JSON.stringify(createData)}</code>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/useCreate.tsx
+++ src/useCreate.tsx
@@ -1,13 +1,49 @@
-import { useState } from 'react'
-import faunadb from 'faunadb'
+import { useCallback, useState } from 'react'
+import faunadb, { query as q } from 'faunadb'
 
-import { FaunaStatus } from './constants'
-import { DataItem } from './types/fauna'
+import { FaunaSchema, FaunaStatus } from './constants'
+// import { DataItem } from './types/fauna'
 
-export default function useCreate(client: faunadb.Client): [Function, null | DataItem, string] {
-  const [data, setData] = useState(null)
-  const [status, setState] = useState(FaunaStatus.NOT_LOADED)
+function createQuery(
+  schema: string,
+  client: faunadb.Client,
+  params: object
+): null | Promise<object> {
+  switch (schema) {
+    case FaunaSchema.Collection:
+      return client.query(q.CreateCollection(params))
+    case FaunaSchema.Database:
+    case FaunaSchema.Document:
+    case FaunaSchema.Function:
+    case FaunaSchema.Index:
+      console.log('schema', schema)
+      break
+  }
+
+  return null
+}
+
+export default function useCreate(client: faunadb.Client): [Function, null | object, string] {
+  const [data, setData] = useState<null | object>(null)
+  const [status, setStatus] = useState(FaunaStatus.NOT_LOADED)
+
+  const create = useCallback((schema: string, params: object) => {
+    const fqlQuery = createQuery(schema, client, params)
+
+    if (fqlQuery) {
+      fqlQuery
+        .then(res => {
+          setStatus(FaunaStatus.LOADING)
+          setData(res)
+          setStatus(FaunaStatus.LOADED)
+        })
+        .catch(err => {
+          console.error(`[fauna-hooks] ${err}`)
+          setStatus(FaunaStatus.ERROR)
+        })
+    }
+  }, [])
 
   console.log(client)
-  return [() => {}, data, status]
+  return [create, data, status]
 }
